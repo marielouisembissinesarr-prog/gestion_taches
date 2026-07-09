ec6ce6d72613efac319e278a0381aad880992af6
Marie Louise Mbissine <marielouisembissine.sarr@unchk.edu.sn>
2026-07-09 23:31:58 +0000

    Ajout de WhiteNoise Middleware

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -52,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'taskmanager.urls'
 
 TEMPLATES = [
