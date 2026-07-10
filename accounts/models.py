0ef49c8f840273a9bbd064c8c6027b1b6bf61b67
Marie Louise Mbissine <marielouisembissine.sarr@unchk.edu.sn>
2026-07-10 12:08:23 +0000

    Fix: Ajout du modèle Tache et mise à jour de la vue

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Tache(models.Model):
+    titre = models.CharField(max_serif="Titre", max_length=200)
+    description = models.TextField(blank=True)
+    terminee = models.BooleanField(default=False)
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titre
+
 # Create your models here.
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Projet
+from .models import Tache  # <-- On importe Tache maintenant !
 
 def home(request):
-    mes_projets = Projet.objects.all()
-    return render(request, 'home.html', {'projets': mes_projets})
+    # On récupère toutes les tâches de la base de données
+    mes_taches = Tache.objects.all()
+    return render(request, 'home.html', {'taches': mes_taches})
 # Create your views here.
