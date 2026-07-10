15cd1d109d8307f9a3d42bf119cbf6a8d32f429a
Marie Louise Mbissine <marielouisembissine.sarr@unchk.edu.sn>
2026-07-10 11:59:27 +0000

    Fix: Nettoyage de la syntaxe dans views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from .models import Projet
 
 def home(request):
-    # On va chercher tous les projets dans la base de données
     mes_projets = Projet.objects.all()
-    # On les envoie à une page HTML (qu'on va appeler home.html)
-    return render(request, 'home.html', {'projets': mes_projets})from django.shortcuts import render
-
+    return render(request, 'home.html', {'projets': mes_projets})
 # Create your views here.
