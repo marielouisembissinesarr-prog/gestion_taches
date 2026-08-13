f0e81cb695e2a7f354ddad15fbb890d56a124fec
Marie Louise Mbissine <marielouisembissine.sarr@unchk.edu.sn>
2026-08-13 21:55:46 +0000

    Nettoyage indentation forms.py

--- projects/forms.py
+++ projects/forms.py
@@ -5,40 +5,38 @@ from .models import Projet, Tache
 
 class ProjetForm(forms.ModelForm):
 
-  class Meta:
-    model = Projet
-    fields = ['titre', 'description', 'membres']
+    class Meta:
+        model = Projet
+        fields = ["titre", "description", "membres"]
 
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    # Rendre la sélection de membres optionnelle pour autoriser les projets solo
-    self.fields['membres'].required = False
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["membres"].required = False
 
 
 class TacheForm(forms.ModelForm):
 
-  class Meta:
-    model = Tache
-    fields = [
-        'titre',
-        'description',
-        'statut',
-        'priorite',
-        'assigne_a',
-        'date_echeance',
-    ]
-   widgets = {
-    'date_echeance': forms.DateInput(
-        attrs={'type': 'date', 'class': 'form-control'}, format='%Y-%m-%d'
-    ),
-}
+    class Meta:
+        model = Tache
+        fields = [
+            "titre",
+            "description",
+            "statut",
+            "priorite",
+            "assigne_a",
+            "date_echeance",
+        ]
+        widgets = {
+            "date_echeance": forms.DateInput(
+                attrs={"type": "date", "class": "form-control"}, format="%Y-%m-%d"
+            ),
+        }
 
-  def __init__(self, *args, **kwargs):
-    projet = kwargs.pop('projet', None)
-    super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        projet = kwargs.pop("projet", None)
+        super().__init__(*args, **kwargs)
 
-    # Si le projet est passé en paramètre, on restreint l'assignation au créateur + membres
-    if projet:
-      self.fields['assigne_a'].queryset = (
-          User.objects.filter(id=projet.createur.id) | projet.membres.all()
-      ).distinct()
+        if projet:
+            self.fields["assigne_a"].queryset = (
+                User.objects.filter(id=projet.createur.id) | projet.membres.all()
+            ).distinct()
